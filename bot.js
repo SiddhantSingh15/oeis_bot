a002ed3f74d6a08062d93ec4a690b66a68ce0911
Siddhant Singh <singhsiddhant@hotmail.co.uk>
2020-09-27 06:12:39 +0300

    Installing New Packages

--- bot.js
+++ bot.js
@@ -1,11 +1,22 @@
 require('dotenv').config();
+const fetch = require('node-fetch');
 
 const {Client} = require('discord.js');
 const client = new Client();
 
+client.once('ready', () =>{
+    console.log('Ready!')
+});
+
 client.on(`message`, (message) => {
-    if (message.content === '!oeis'){
-        message.reply('Please type your sequence: ')
+    if (message.content.includes('oeis-')){
+        let userReq = message.content;
+        let sequence = userReq.replace('oeis-','');
+        let fullURL = 'http://oeis.org/search?q=' + encodeURIComponent(sequence) + '&sort=&language=english&go=Search';
+        fetch(fullURL).then(res => res.text());
+        console.log('Message sent by: ' + message.author.username);
+        console.log('Sequence requested: ' + sequence);
+        console.log('URL: ' + fullURL)
     }
 });
 
