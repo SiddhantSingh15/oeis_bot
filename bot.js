3d580b752782dcba63e8bca7637dfb9c7d0b99c2
Siddhant Singh <singhsiddhant@hotmail.co.uk>
2020-09-28 20:47:34 +0300

    Adding web scraping module
    - Using axios
    - Outputting in required format

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
-const fetch = require('node-fetch');
-
 const {Client} = require('discord.js');
+const axios = require('axios');
 const client = new Client();
 
 client.once('ready', () =>{
@@ -11,9 +10,21 @@ client.once('ready', () =>{
 client.on(`message`, (message) => {
     if (message.content.includes('oeis-')){
         let userReq = message.content;
-        let sequence = userReq.replace('oeis-','');
-        let fullURL = 'http://oeis.org/search?q=' + encodeURIComponent(sequence) + '&sort=&language=english&go=Search';
-        fetch(fullURL).then(res => res.text());
+        let sequence = userReq.replace('oeis-','')
+        let fullURL = 'https://oeis.org/search?fmt=json&q=%3C'+sequence+'%3E&start=%3CitemToStartAt%3E'
+        let displayLink = 'http://oeis.org/search?q=' + encodeURI(sequence) + '&sort=&language=&go=Search'
+
+        axios(fullURL)
+            .then(function (html){
+                const desiredObj = html.data.results[0];
+                const indexNo = 'A'+desiredObj.number;
+                const description = desiredObj.name;
+                message.reply(`\n**OEIS Index: ** ${indexNo} \n**Description : ** ${description} \n**Find more at: ** ${displayLink}`)
+            })
+            .catch(function (err){
+                console.log('error found')
+            })
+
         console.log('Message sent by: ' + message.author.username);
         console.log('Sequence requested: ' + sequence);
         console.log('URL: ' + fullURL)
